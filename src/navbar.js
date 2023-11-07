b5ab78c9f1f72b148c0ff9743d51c9fc856fe3e1
ktorr <103144674+ktorr@users.noreply.github.com>
2023-11-07 13:06:13 -0500

    Create navbar.js

---
+++ src/navbar.js
@@ -0,0 +1,33 @@
+import {Link} from 'react-router-dom';
+import "./navbar.css";
+
+function Navbar() {
+  return (
+    <>
+        <nav className="navbar">
+          <div className="navbar-container">
+            <Link to="/" className="navbar-logo" >GeoSync</Link>
+            <ul className="nav-menu">
+              <li className="nav-item">
+                <Link to="/" className="nav-links" >Map</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/wiki" className="nav-links" >Wiki</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/documents" className="nav-links" >Documents</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact-us" className="nav-links" >Contact Us</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/FAQ" className="nav-links" >FAQs</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
