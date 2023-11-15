be5c74cef626f64b1c7696eed47ce25aaf30ed90
Kevin <vukevin0212@gmail.com>
2023-11-15 11:26:30 -0500

    added routing to differen fragments within app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import './App.css';
 import Navbar from './components/navbar';
+import Home from './pages/Home';
+import Contact from './fragments/Contact';
+import Documents from './fragments/Documents';
+import FAQ from './fragments/FAQ';
+import Map from './fragments/Map';
+import Wiki from './fragments/Wiki';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
@@ -8,6 +14,13 @@ function App() {
       <Router>
         <Navbar/>
           <Routes>
+            <Route exact path="/" element={<Home/>}></Route>
+            <Route exact path="/Contact" element={<Contact/>}></Route>
+            <Route exact path="/Documents" element={<Documents/>}></Route>
+            <Route exact path="/FAQ" element={<FAQ/>}></Route>
+            <Route exact path="/Map" element={<Map/>}></Route>
+            <Route exact path="/Wiki" element={<Wiki/>}></Route>
+
           </Routes>
       </Router>
     </div>
