f692386a71d2532f76aa707e5f88da5a099332a5
ktorr <kennedytorrent@gmail.com>
2024-01-10 15:43:43 -0500

    map with filters and homepage

---
+++ src/components/fragments/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Home.css";
+
+function Home() {
+   function navigateToMap() {
+      window.location.href = '/map';
+    }
+   return (  
+      <div className="columns">    
+         <div>Mission Statement</div>
+         <div>Tasks</div>
+         <div className="map-img" onClick={() => navigateToMap()}></div>
+      </div>
+   )
+   
+}
+
+export default Home
--- src/components/fragments/Map.js
+++ src/components/fragments/Map.js
@@ -1,7 +1,19 @@
-import React, {useRef, useEffect} from 'react';
+import React, {useState, useMemo, useRef, useEffect} from 'react';
 import {loadModules} from 'esri-loader';
+import Select from 'react-select';
+import countryList from 'react-select-country-list';
+import './Map.css';
 
 function Map() {
+    // Country selector
+    const [value, setValue] = useState('')
+    const options = useMemo(() => countryList().getData(), [])
+  
+    const changeHandler = value => {
+      setValue(value)
+    }
+
+    // Map
     const mapElem = useRef(null);
 
     useEffect(()=>{
@@ -13,7 +25,7 @@ function Map() {
             view = new MapView({
                 map: webmap,
                 center: [0,0],
-                zoom:2,
+                zoom: 2,
                 container: mapElem.current
             })
         })
@@ -25,9 +37,95 @@ function Map() {
         }
     })
 
-    return (
-        <div style={{height:500}} ref={mapElem}>
+    // PMESII-PT filters
+    const [checkboxes, setCheckboxes] = useState({
+        checkbox1: true,
+        checkbox2: true,
+        checkbox3: true,
+        checkbox4: true,
+        checkbox5: true,
+        checkbox6: true
+      });
+    
+      const handleCheckboxChange = (checkboxName) => {
+        setCheckboxes((prevCheckboxes) => ({
+          ...prevCheckboxes,
+          [checkboxName]: !prevCheckboxes[checkboxName],
+        }));
+      };
 
+
+    return (
+        <div className="home">
+            <div className="filters-container">
+                <h3>PMESII Filters</h3>
+                <form className="filters-form">
+                    <div className="filter">
+                        <label>
+                            <input
+                            type="checkbox"
+                            checked={checkboxes.checkbox1}
+                            onChange={() => handleCheckboxChange('checkbox1')}
+                            />
+                            Political
+                        </label>
+                    </div>
+                    <div className="filter">
+                        <label>
+                            <input
+                            type="checkbox"
+                            checked={checkboxes.checkbox2}
+                            onChange={() => handleCheckboxChange('checkbox2')}
+                            />
+                            Military
+                        </label>
+                    </div>
+                    <div className="filter">
+                        <label>
+                            <input
+                            type="checkbox"
+                            checked={checkboxes.checkbox3}
+                            onChange={() => handleCheckboxChange('checkbox3')}
+                            />
+                            Economic
+                        </label>
+                    </div>
+                    <div className="filter">
+                        <label>
+                            <input
+                            type="checkbox"
+                            checked={checkboxes.checkbox4}
+                            onChange={() => handleCheckboxChange('checkbox4')}
+                            />
+                            Social
+                        </label>
+                    </div>
+                    <div className="filter">
+                        <label>
+                            <input
+                            type="checkbox"
+                            checked={checkboxes.checkbox5}
+                            onChange={() => handleCheckboxChange('checkbox5')}
+                            />
+                            Information
+                        </label>
+                    </div>
+                    <div className="filter">
+                        <label>
+                            <input
+                            type="checkbox"
+                            checked={checkboxes.checkbox6}
+                            onChange={() => handleCheckboxChange('checkbox6')}
+                            />
+                            Infrastructure
+                        </label>         
+                    </div>       
+                </form>
+            </div>
+            <div className="map-container">
+                <Select placeholder="Select a Country" options={options} value={value} onChange={changeHandler} />
+                <div className="map" ref={mapElem}></div>
+            </div>
         </div>
     )
 }
