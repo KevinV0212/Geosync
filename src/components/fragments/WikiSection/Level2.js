c12e562b490fbabcea15cd79de59f275093703de
ktorr <kennedytorrent@gmail.com>
2024-01-23 14:45:08 -0500

    Map and wiki css corrected

--- src/components/fragments/WikiSection/Level2.js
+++ src/components/fragments/WikiSection/Level2.js
@@ -21,28 +21,4 @@ function Level2 (props) {
     )
 }
 
-export default Level2
-import React from "react";
-import {testEntries} from "../../../test_data/wikiTest";
-
-//Passing in PMESII and ASCOPE
-function Level2 (props) {
-
-    return (
-        <div>
-            {testEntries.filter((element) => element[props.PMESII] === true && element[props.ASCOPE] === true).map((item, i) => (
-                <div className='element'>
-                    <div className='individualTitle'>
-                        <h4><b>{item.Title}</b></h4>           
-                    </div>
-                    <div className='individualContent'>
-                        <>{item.Description}</>
-                    </div>
-                </div>
-            
-            ))}
-        </div>
-    )
-}
-
-export default Level2
+export default Level2;
