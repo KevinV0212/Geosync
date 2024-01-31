4b56df87188e873986acc3946fbca9a17092657b
ktorr <kennedytorrent@gmail.com>
2024-01-31 12:05:56 -0500

    populated contact us

--- src/components/fragments/Contact.js
+++ src/components/fragments/Contact.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import "./Contact.css"
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className='div'>
+      <h2 className='heading'>CONTACT INFORMATION</h2>
+      <h2 className='lineborder'>General Assistance</h2>
+      <h3 className='name'>Fort Liberty Automated Operator</h3>
+      <p className='info'>(910) 396-0011</p>
+      <p className='info'>&nbsp;</p>
+      <h3 className='name'>USASOC Command Center</h3>
+      <p className='info'>(910) 3432-7001</p>
+      <h2 className='lineborder'>USASOC Address</h2>
+      <p className='info'>2929 Desert Storm Drive</p>
+      <p className='info'>Fort Liberty, NC 28310-9110</p>
+      <h2 className='lineborder'>Freedom of Information Act</h2>
+      <h3 className='name'>Freedom of Information Act Office</h3>
+      <p className='info'>USASOC (AOIM-FOIA)</p>
+      <p className='info'>2929 Desert Storm Drive</p>
+      <p className='info'>Fort Liberty, NC 28310-9110</p>
+      <p className='info'>&nbsp;</p>
+      <h3 className='name'>FOIA Officer</h3>
+      <p className='info'>(910) 432-9233</p>
+      <p className='info'>&nbsp;</p>
+      <h3 className='name'>FOIA Analysts</h3>
+      <p className='info'>(910) 432-9107</p>
+      <p className='info'>(910) 432-2437</p>
+      <h2 className='lineborder'>Media Contact Information</h2>
+      <h3 className='name'>Public Affairs Office</h3>
+      <p className='info'>(910) 432-6005</p>
+      <p className='info'>PAO-USASOC@socom.mil</p>
+      <p className='info'>&nbsp;</p>
+    </div>
   )
 }
