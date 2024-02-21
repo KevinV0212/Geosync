66ace7658f223a74be12d8dc472d61d6c6da8aad
ktorr <kennedytorrent@gmail.com>
2024-02-21 17:02:01 -0500

    button styles

--- src/components/fragments/MapFragment.js
+++ src/components/fragments/MapFragment.js
@@ -119,7 +119,7 @@ function Map() {
       <div className="home">
          <div className="filters-container">
             {renderManagerControls()}
-            <Button onClick={handleViewChange}>
+            <Button variant="outlined" size="small" sx={{ marginTop: 0.5, color: '#690005', border: '1px solid #690005', width: '126.37px' }} onClick={handleViewChange}>
                {managerView ? "user view" : "manager view"}
             </Button>
             <h3>PMESII Filters {mapPins.length}</h3>
