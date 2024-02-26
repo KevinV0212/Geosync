c6d229a37de200ba03d67ba1e9a5329ac63d8143
IethaIyy <sebastianperez46840@gmail.com>
2024-02-26 13:46:14 -0500

    Fixing Latitude/Longitude swap

--- src/test_data/pinTest.js
+++ src/test_data/pinTest.js
@@ -2,8 +2,8 @@
 // data is for Vatican city
 
 // Coordinates
-const mapLong = 41.9029;
-const mapLat = 12.4534;
+const mapLat = 41.9029;
+const mapLong = 12.4534;
 
 // pins for each letter of PMESII
 const testPins = [];
@@ -14,8 +14,8 @@ testPins.push({
   Title: "Apostolic Palace",
   Description:
     "The Apostolic Palace (Latin: Palatium Apostolicum; Italian: Palazzo Apostolico) is the official residence of the Pope, the head of the Catholic Church, located in Vatican City. ",
-  Longitude: 41.903611,
-  Latitude: 12.456389,
+  Latitude: 41.903611,
+  Longitutde: 12.456389,
   Political: true,
   Military: false,
   Economic: false,
@@ -31,8 +31,8 @@ testPins.push({
   Title: "Arch of the Bells",
   Description:
     "Arch of the Bells is a city gate in Municipio Roma XIV, Rome, Lazio. Arch of the Bells is situated nearby to the post office Poste Vaticane and the square Basilica Forecourt.",
-  Longitude: 41.90181285056788,
-  Latitude: 12.45490191086849,
+  Latitude: 41.90181285056788,
+  Longitutde: 12.45490191086849,
   Political: false,
   Military: true,
   Economic: false,
@@ -48,8 +48,8 @@ testPins.push({
   Title: "Institute for Works of Religion",
   Description:
     "The Institute for the Works of Religion, commonly known as the Vatican Bank, is a financial institution that is situated inside Vatican City and run by a Board of Superintendence, which reports to a Commission of Cardinals and the Pope. ",
-  Longitude: 41.90392764880936,
-  Latitude: 12.456710497480108,
+  Latitude: 41.90392764880936,
+  Longitutde: 12.456710497480108,
   Political: false,
   Military: false,
   Economic: true,
@@ -65,8 +65,8 @@ testPins.push({
   Title: "St Peter's Basilica",
   Description:
     "The Papal Basilica of Saint Peter in the Vatican, or simply Saint Peter's Basilica, is a church built in the Renaissance style located in Vatican City, the papal enclave that is within the city of Rome, Italy.",
-  Longitude: 41.902238539127566,
-  Latitude: 12.45394742493224,
+  Latitude: 41.902238539127566,
+  Longitutde: 12.45394742493224,
   Political: false,
   Military: false,
   Economic: false,
@@ -82,8 +82,8 @@ testPins.push({
   Title: "Vatican City Heliport",
   Description:
     "Vatican City Heliport consists of a 25 × 17 m rectangular concrete landing area linked with a circular parking area. It is used for short journeys from or to Vatican City by the pope and visiting heads of state",
-  Longitude: 41.902042409599204,
-  Latitude: 12.44643048878754,
+  Latitude: 41.902042409599204,
+  Longitutde: 12.44643048878754,
   Political: false,
   Military: false,
   Economic: false,
@@ -97,8 +97,8 @@ testPins.push({
   Country: "Vatican City",
   Title: "Poste Vaticane (centrale)",
   Description: "Post office in the center of Vatican City",
-  Longitude: 41.90432369914862,
-  Latitude: 12.456147610805392,
+  Latitude: 41.90432369914862,
+  Longitutde: 12.456147610805392,
   Political: false,
   Military: false,
   Economic: false,
