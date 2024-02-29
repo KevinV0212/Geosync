786529bde5a8d4abe241f787fbefc5ac978b19d4
ktorr <kennedytorrent@gmail.com>
2024-02-29 15:03:42 -0500

    added the theme provider

--- src/components/controls/Button.js
+++ src/components/controls/Button.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { Button as MuiButton } from "@mui/material";
+import { Button as MuiButton, ThemeProvider } from "@mui/material";
+import theme from "./Theme";
+
 export default function Button(props) {
    const { text, size, color, variant, onClick, ...other } = props;
 
    return (
-      <MuiButton
-         variant={variant || "contained"}
-         size={size || "large"}
-         color={color || "primary"}
-         onClick={onClick}
-         {...other}
-      >
-         {text}
-      </MuiButton>
+      <ThemeProvider theme={theme}>
+         <MuiButton
+            variant={variant || "contained"}
+            size={size || "large"}
+            color={color || "primary"}
+            onClick={onClick}
+            {...other}
+         >
+            {text}
+         </MuiButton>
+      </ThemeProvider>
    );
 }
