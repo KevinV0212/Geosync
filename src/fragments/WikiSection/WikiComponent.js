e5ed30b1a556bfd72e09038910acc7f1538998f7
Jack Price <pricej2420@outlook.com>
2024-03-14 16:48:32 -0400

    Wiki code changes

---
+++ src/fragments/WikiSection/WikiComponent.js
@@ -0,0 +1,108 @@
+import { Box, ThemeProvider } from "@mui/system";
+import React, { useEffect, useState } from "react";
+import { Typography } from "@mui/material";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import Divider from "@mui/material/Divider";
+import { testEntries } from "../../test_data/wikiTest";
+
+//selectedPMESII and selectedASCOPE useStates
+//Entries
+export default function WikiComponent (props) {
+
+    const PMESII = [
+        "Political",
+        "Military",
+        "Economic",
+        "Social",
+        "Information",
+        "Infrastructure",
+    ];
+    const ASCOPE = [
+        "Areas",
+        "Structure",
+        "Capabilities",
+        "Organization",
+        "People",
+        "Events",
+    ];
+
+    return (
+        <Box
+            component="main"
+            sx={{
+            flexGrow: 1,
+            background: "#D7CEC7",
+            overflowY: "scroll",
+            maxHeight: "75vh",
+            }}
+        >
+            {PMESII.map((currentPMESII, i) => (
+            <div key={i}>
+                {props.selectedPMESII[i] && (
+                    <Accordion
+                        defaultExpanded
+                        sx={{ border: 1, borderColor: "black" }}
+                    >
+                        <AccordionSummary
+                        expandIcon={<ArrowDropDownIcon />}
+                        aria-controls="panel1-content"
+                        id="panel1-header"
+                        >
+                        <Typography sx={{ fontWeight: "bold" }}>
+                            {currentPMESII}
+                        </Typography>
+                        </AccordionSummary>
+                        <Divider
+                        variant="middle"
+                        sx={{
+                            borderBottomWidth: 1,
+                            borderColor: "black",
+                        }}
+                        />
+                        <AccordionDetails>
+                        {ASCOPE.map((currentASCOPE, j) => (
+                        <div key={i}>
+                        {props.selectedASCOPE[i * 6 + j] && (
+                            <Accordion>
+                                <AccordionSummary
+                                    expandIcon={<ArrowDropDownIcon />}
+                                    aria-controls="panel1-content"
+                                    id="panel1-header"
+                                >
+                                    <Typography>{currentASCOPE}</Typography>
+                                </AccordionSummary>
+                                <Divider
+                                    variant="middle"
+                                    sx={{ borderBottomWidth: 1, borderColor: "black" }}
+                                />
+                                <AccordionDetails>
+                                {props.entries
+                                .filter(
+                                (element) =>
+                                    element[currentPMESII] === true &&
+                                    element[currentASCOPE] === true
+                                )
+                                .map((entry, k) => (
+                                <Box sx={{}} key={k}>
+                                    <Typography sx={{ textDecoration: "underline" }}>
+                                        {entry.Title}
+                                    </Typography>
+                                    <Typography>{entry.Description} </Typography>
+                                </Box>
+                                ))}
+                                </AccordionDetails>
+                            </Accordion>
+                        )}
+                        </div>
+                        ))}
+                        </AccordionDetails>
+                    </Accordion>
+                )}
+            </div>
+            ))}
+        </Box>
+    )
+}
--- src/fragments/wiki/Wiki.js
+++ src/fragments/wiki/Wiki.js
@@ -1,12 +1,13 @@
 import React from "react";
 import WikiSection from "../WikiSection/WikiSection";
 import EntryForm from "../../components/forms/EntryForm";
+import WikiFragment from "./WikiFragment";
 
 function Wiki() {
    return (
       <div>
          {/* <EntryForm /> */}
-         <WikiSection />
+         <WikiFragment />
       </div>
    );
 }
