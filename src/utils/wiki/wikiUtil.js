ed4a58374933e6d19b225bb5a3a7271f39d84ab8
Kevin <vukevin0212@gmail.com>
2024-03-24 23:22:43 -0400

    added wiki update

--- src/utils/wiki/wikiUtil.js
+++ src/utils/wiki/wikiUtil.js
@@ -126,7 +126,47 @@ async function addWikiEntry(requestBody) {
    return response.data;
 }
 
-async function updateWikiEntry(requestBody) {}
+async function updateWikiEntry(requestBody) {
+   if (!requestBody) {
+      return null;
+   }
+   const url = buildPath("/update_wiki");
+   let obj = requestBody;
+
+   let config = {
+      method: "put",
+      url: url,
+      headers: {
+         headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers":
+               "Origin, X-Requested-With, Content-Type, Accept",
+            "Content-Type": "application/json",
+         },
+      },
+      data: obj,
+   };
+
+   const response = await axios.request(config).catch((error) => {
+      if (error.response) {
+         // The request was made and the server responded with a status code
+         // that falls out of the range of 2xx
+         console.log(error.response.data);
+         console.log(error.response.status);
+         console.log(error.response.headers);
+      } else if (error.request) {
+         // The request was made but no response was received
+         // `error.request` is an instance of XMLHttpRequest in the browser
+         // and an instance of http.ClientRequest in node.js
+         console.log(error.request);
+      } else {
+         // Something happened in setting up the request that triggered an Error
+         console.log("Error", error.message);
+      }
+      return { data: null };
+   });
+   return response.data;
+}
 
 // deletes map pin with given mapPinID
 async function deleteWikiEntry(wikiEntryID) {
